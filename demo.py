697cd697e772dccfe93ea41035bb26b6844f73bb
Dhruv Tyagi <dhruvtyagi203@gmail.com>
2023-08-13 21:15:21 +0530

    final commit

--- demo.py
+++ demo.py
@@ -3,11 +3,11 @@ from src.components.data_transformation import DataTransformation
 from src.pipelines.training_pipeline import train_pipeline
 from src.components.model_trainer import ModelTrainer
 
-ing = DataIngestion()
-train_path, test_path = ing.initiate_data_ingestion()
+# ing = DataIngestion()
+# train_path, test_path = ing.initiate_data_ingestion()
 
-trans = DataTransformation()
-trans.initiate_data_transformation(train_path, test_path)
+# trans = DataTransformation()
+# trans.initiate_data_transformation(train_path, test_path)
 
 train_arr,test_arr=train_pipeline.start_training_pipeline()
 
